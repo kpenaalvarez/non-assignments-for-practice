a364b216d6b0188672697896c0481c7f86c47758
kpenaalvarez <136401504+kpenaalvarez@users.noreply.github.com>
2023-06-20 17:55:13 -0700

    Add files via upload

---
+++ cube problem.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 20 17:40:46 2023
+
+@author: Kpenaalvarez
+
+set up a cube with side lenght = 4
+find and print the volume of a cube 
+V and L are variables
+"""
+
+L = 4
+
+# find the volume:
+V = L*L*L
+# alternately
+V = L**3
+
+print(f"\nLength, L = {L}")
+print(f"\nVolume = {V}")
+
+normalString = 'this is a normal string'
+
+print(f"\nNormal string: {normalString}")
+A = normalString.split()
+print(A)
