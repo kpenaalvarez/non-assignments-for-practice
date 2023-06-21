ec13ebae3542d775e547222dd2dcd1ac73c25da3
kpenaalvarez <136401504+kpenaalvarez@users.noreply.github.com>
2023-06-20 18:54:29 -0700

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 20 18:19:53 2023
+
+@author: Kpenaalvarez
+"""
+
+import pandas as pd
+
+myDict = {"one": "the number 1",
+          "two": "2"}
+
+#if you type in Mydict["two"] or myDict["one"] in the console 
+#then you get what you defines "one" and "two" as
