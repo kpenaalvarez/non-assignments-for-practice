7028cfd2d1bb71cbf4893d36e3d871e9b685bc50
kpenaalvarez <136401504+kpenaalvarez@users.noreply.github.com>
2023-06-20 18:19:50 -0700

    Add files via upload

---
+++ for i in range, loop.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 20 17:55:36 2023
+
+@author: Kpenaalvarez
+
+"""
+m = 10
+for i in range(1 ,10, 3):
+    print(i)
+# for i in range(1, 10, 2) is used for loops first
+#number is 1 through 9, and the 2 tells it to do 
+#odd numbers
+
+condition = True
+i = 0
+while (condition):
+    print(i)
+    i += 1
+    if i > 20: condition = False
+# += does 
+
+
